c664c363f4efcf059a86ebad1880407705bd312c
Park Yong Ha <yonghapk@gmail.com>
2024-11-10 20:59:01 +0900

    add server side fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,21 +1,18 @@
-"use client";
-
 import { useEffect, useState } from "react";
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
+export const metadata = {
+  title: "HOME",
+};
 
-  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
