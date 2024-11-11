34edef54db42c2b58bfafb699b66ee23bb106203
Park Yong Ha <yonghapk@gmail.com>
2024-11-11 23:48:26 +0900

    add loading component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,6 +7,7 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(URL);
   const json = await response.json();
   return json;
