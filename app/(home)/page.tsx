6c36f56b2667d1fa7a7f0264d841fc9cd2cce00c
Park Yong Ha <yonghapk@gmail.com>
2024-11-12 23:59:02 +0900

    add sub loading

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,25 @@
-import { useEffect, useState } from "react";
-
 export const metadata = {
   title: "HOME",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(URL);
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li>
+          <a href={`/movies/${movie.id}`}>{movie.title}</a>
+        </li>
+      ))}
+    </div>
+  );
 }
