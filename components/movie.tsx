2f6887c0ea1d6f8a78eebf4e89cceb0138d5f851
Park Yong Ha <yonghapk@gmail.com>
2024-11-20 23:25:43 +0900

    add router

--- components/movie.tsx
+++ components/movie.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import styles from "../styles/movie.module.css";
+import { useRouter } from "next/navigation";
 
 interface IMovieProps {
   title: string;
@@ -8,9 +11,13 @@ interface IMovieProps {
 }
 
 export default function Movie({ title, id, poster_path }: IMovieProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/movies/${id}`);
+  };
   return (
     <div className={styles.movie}>
-      <img src={poster_path} alt={title} />
+      <img src={poster_path} alt={title} onClick={onClick} />
       <Link href={`/movies/${id}`}>{title}</Link>
     </div>
   );
