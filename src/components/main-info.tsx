605863b34f2a11a82c2c0cafc763937d84b521d4
Park Yong Ha <yonghapk@gmail.com>
2025-01-20 06:48:22 +0900

    set querydate

--- src/components/main-info.tsx
+++ src/components/main-info.tsx
@@ -2,10 +2,38 @@
 import * as common from "@/styles/common.css";
 import * as code from "@/constants/code";
 import * as icon_weather from "@/assets/images/icon_weather/index";
+import { QueryClient } from "@tanstack/react-query";
 
 export async function getCurrentWeather() {
+  // 오늘 날짜 구하기 0000
+  let targetDate = new Date();
+  const forecastTime = [2, 5, 8, 11, 14, 17, 20, 23];
+  const currentHours = targetDate.getHours(); // 14
+  let target = 0;
+  for (var i = 0; i < forecastTime.length; i++) {
+    if (currentHours <= 1) {
+      targetDate = new Date(targetDate.setDate(targetDate.getDate() - 1));
+      target = 23;
+      break;
+    } else if (currentHours <= forecastTime[i]) {
+      target = forecastTime[i - 1];
+      break;
+    }
+  }
+  const year = targetDate.getFullYear();
+  const month = targetDate.getMonth() + 1;
+  const day = targetDate.getDate();
+  const newDay =
+    ("00" + month.toString()).slice(-2) + ("00" + day.toString()).slice(-2);
+  const queryDate = year + newDay;
+  const queryTime = ("00" + target.toString()).slice(-2) + "00";
+
   const test = await fetch(
-    "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=hhPRU4TihqC7sGrFL7uNTmty4I7Hng2A57yNkCPaRsb%2BbnlxyetnLDADCFy%2FDh0KshzZmRBEyFO1VEMKNHeuPg%3D%3D&numOfRows=10&pageNo=1&base_date=20250119&base_time=0500&nx=55&ny=127&dataType=json"
+    "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=hhPRU4TihqC7sGrFL7uNTmty4I7Hng2A57yNkCPaRsb%2BbnlxyetnLDADCFy%2FDh0KshzZmRBEyFO1VEMKNHeuPg%3D%3D&numOfRows=10&pageNo=1&base_date=" +
+      queryDate +
+      "&base_time=" +
+      queryTime +
+      "&nx=55&ny=127&dataType=json"
   );
   const json = await test.json();
   const data = json.response.body;
@@ -16,7 +44,7 @@ export default async function MainInfo() {
   const info = await getCurrentWeather();
   return (
     <div className={common.box}>
-      <div className={common.info}>
+      <div>
         <div className={common.info__wrap}>
           <div className={common.info__icon}>
             {info.map((i) => {
