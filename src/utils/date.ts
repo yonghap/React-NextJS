77972e312347da6a0d12cf5c06d57ee7319e631e
Park Yong Ha <yonghapk@gmail.com>
2025-02-26 02:33:30 +0900

    add Hourly api

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -7,38 +7,38 @@ const getToday = (): string => {
     ("0" + today.getDate()).slice(-2)
   );
 };
-const addZero = (num: number): string => {
+const addZeroTime = (num: number): string => {
   return ("00" + num.toString()).slice(-2) + "00";
 };
+
 // 단기 예보 시간 날짜 구하기
-export const getShortRangDate = (): array => {
+export const getShortRangeDate = (): Array<string> => {
   const today = new Date();
   const currentHours = today.getHours();
   const currentMinutes = today.getMinutes();
   const forecastTimes = [2, 5, 8, 11, 14, 17, 20, 23];
   const isCurrentAPI = currentMinutes >= 15 ?? false;
+  const isYesterDay = (currentHours <= 2 && !isCurrentAPI) ?? false;
 
-  if (currentHours <= 2 && !isCurrentAPI) {
+  if (isYesterDay) {
     const yesterDayDate = new Date(today.setDate(today.getDate() - 1));
     return [
       yesterDayDate.getFullYear() +
         ("0" + (1 + yesterDayDate.getMonth())).slice(-2) +
-        ("0" + yesterDayDate.getDate()).slice(-2),
+        addZeroTime(yesterDayDate.getDate()),
       "2300",
     ];
   } else {
-    forecastTimes.forEach((time, index) => {
-      console.log("dddddddd", addZero(forecastTimes[index]));
-      if (currentHours === time) {
+    for (let i = 0; i < forecastTimes.length; i++) {
+      if (currentHours === forecastTimes[i]) {
         if (isCurrentAPI) {
-          return [getToday(), addZero(time)];
+          return [getToday(), addZeroTime(forecastTimes[i])];
         } else {
-          return [getToday(), addZero(forecastTimes[index - 1])];
+          return [getToday(), addZeroTime(forecastTimes[i - 1])];
         }
-      } else if (currentHours < time) {
-        return [getToday(), addZero(forecastTimes[index - 1])];
+      } else if (currentHours < forecastTimes[i]) {
+        return [getToday(), addZeroTime(forecastTimes[i - 1])];
       }
-    });
+    }
   }
-  // if (currentHours <= 2) forecastTimes.forEach((i) => {});
 };
